895d428e4d89d65d18e49524615616cdf7fb31fb
mind <peace0phmind@gmail.com>
2024-01-07 15:47:19 +0800

    Add comprehensive tests for methods in `stream_test.go`.

--- stream/stream_test.go
+++ stream/stream_test.go
@@ -188,6 +188,421 @@ func TestStream_Shuffle(t *testing.T) {
 	}
 }
 
+func Test_Stream_Limit(t *testing.T) {
+	testCases := []struct {
+		name      string
+		input     Stream[int]
+		inputN    int
+		want      Stream[int]
+		wantError bool
+	}{
+		{
+			name:      "LimitNegative",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    -2,
+			want:      Of([]int{}),
+			wantError: false,
+		},
+		{
+			name:      "LimitZero",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    0,
+			want:      Of([]int{}),
+			wantError: false,
+		},
+		{
+			name:      "LimitInRange",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    3,
+			want:      Of([]int{1, 2, 3}),
+			wantError: false,
+		},
+		{
+			name:      "LimitUpperRange",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    5,
+			want:      Of([]int{1, 2, 3, 4, 5}),
+			wantError: false,
+		},
+		{
+			name:      "LimitOverflow",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    10,
+			want:      Of([]int{1, 2, 3, 4, 5}),
+			wantError: false,
+		},
+		{
+			name:      "LimitError",
+			input:     Stream[int]{err: errors.New("example error")},
+			inputN:    2,
+			want:      Stream[int]{err: errors.New("example error")},
+			wantError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.input.Limit(tc.inputN)
+
+			// Check error cases
+			if (got.err != nil) != tc.wantError {
+				t.Errorf("Stream[int].Limit() error = %v, wantError %v", got.err, tc.wantError)
+				return
+			}
+			if got.err != nil && tc.wantError {
+				if got.err.Error() != tc.want.err.Error() {
+					t.Errorf("Stream[int].Limit() error = %v, wantError %v", got.err, tc.want.err)
+				}
+				return
+			}
+
+			// Check valid cases
+			gotSlice, _ := got.ToSlice()
+			wantSlice, _ := tc.want.ToSlice()
+			if !reflect.DeepEqual(gotSlice, wantSlice) {
+				t.Errorf("Stream[int].Limit() = %v, want %v", gotSlice, wantSlice)
+			}
+		})
+	}
+}
+
+func TestStream_Skip(t *testing.T) {
+	testCases := []struct {
+		name      string
+		input     Stream[int]
+		inputN    int
+		want      Stream[int]
+		wantError bool
+	}{
+		{
+			name:      "skip 0",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    0,
+			want:      Of([]int{1, 2, 3, 4, 5}),
+			wantError: false,
+		},
+		{
+			name:      "skip all elements",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    5,
+			want:      Of([]int{}),
+			wantError: false,
+		},
+		{
+			name:      "skip 3 elements",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    3,
+			want:      Of([]int{4, 5}),
+			wantError: false,
+		},
+		{
+			name:      "skip more than length",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    10,
+			want:      Of([]int{}),
+			wantError: false,
+		},
+		{
+			name:      "negative skip",
+			input:     Of([]int{1, 2, 3, 4, 5}),
+			inputN:    -1,
+			want:      Of([]int{1, 2, 3, 4, 5}),
+			wantError: false,
+		},
+		{
+			name:      "skip with error",
+			input:     Stream[int]{elems: []int{1, 2, 3, 4, 5}, err: errors.New("skip error")},
+			inputN:    2,
+			want:      Stream[int]{err: errors.New("skip error")},
+			wantError: true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.input.Skip(tc.inputN)
+
+			// Check error cases
+			if (got.err != nil) != tc.wantError {
+				t.Errorf("Stream[int].Skip() error = %v, wantError %v", got.err, tc.wantError)
+				return
+			}
+			if got.err != nil && tc.wantError {
+				if got.err.Error() != tc.want.err.Error() {
+					t.Errorf("Stream[int].Skip() error = %v, wantError %v", got.err, tc.want.err)
+				}
+				return
+			}
+
+			// Check valid cases
+			gotSlice, _ := got.ToSlice()
+			wantSlice, _ := tc.want.ToSlice()
+			if !reflect.DeepEqual(gotSlice, wantSlice) {
+				t.Errorf("Stream[int].Skip() = %v, want %v", gotSlice, wantSlice)
+			}
+		})
+	}
+}
+
+func TestAllMatch(t *testing.T) {
+	testCases := []struct {
+		name  string
+		elems []int
+		match func(int) (bool, error)
+		want  bool
+	}{
+		{
+			"empty slice",
+			[]int{},
+			func(i int) (bool, error) { return i > 0, nil },
+			true,
+		},
+		{
+			"all match",
+			[]int{1, 2, 3},
+			func(i int) (bool, error) { return i > 0, nil },
+			true,
+		},
+		{
+			"some not match",
+			[]int{1, -1, 3},
+			func(i int) (bool, error) { return i > 0, nil },
+			false,
+		},
+		{
+			"none match",
+			[]int{-1, -2, -3},
+			func(i int) (bool, error) { return i > 0, nil },
+			false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			s := Stream[int]{elems: tc.elems}
+			got, err := s.AllMatch(tc.match)
+			if err == nil && got != tc.want {
+				t.Errorf("AllMatch got = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestStream_AnyMatch(t *testing.T) {
+	type testCase struct {
+		name           string
+		stream         Stream[int]
+		matchFunc      func(int) (bool, error)
+		expectedResult bool
+		expectError    bool
+	}
+
+	testCases := []testCase{
+		{
+			name:           "Match Found",
+			stream:         Stream[int]{elems: []int{1, 2, 3, 4, 5}},
+			matchFunc:      func(n int) (bool, error) { return n > 3, nil },
+			expectedResult: true,
+			expectError:    false,
+		},
+		{
+			name:           "Match Not Found",
+			stream:         Stream[int]{elems: []int{1, 2, 3, 4, 5}},
+			matchFunc:      func(n int) (bool, error) { return n > 5, nil },
+			expectedResult: false,
+			expectError:    false,
+		},
+		{
+			name:           "Error In Match Function",
+			stream:         Stream[int]{elems: []int{1, 2, 3, 4, 5}},
+			matchFunc:      func(n int) (bool, error) { return false, errors.New("Unknown Error") },
+			expectedResult: false,
+			expectError:    true,
+		},
+		{
+			name:           "Empty Stream",
+			stream:         Stream[int]{elems: []int{}},
+			matchFunc:      func(n int) (bool, error) { return n > 3, nil },
+			expectedResult: false,
+			expectError:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.stream.AnyMatch(tc.matchFunc)
+			if err != nil && !tc.expectError {
+				t.Fatalf("expected no error, but got: %v", err)
+			}
+			if err == nil && tc.expectError {
+				t.Fatalf("expected error, but got none")
+			}
+			if result != tc.expectedResult {
+				t.Fatalf("expected %v, but got %v", tc.expectedResult, result)
+			}
+		})
+	}
+}
+
+func compareInts(x, y int) (int, error) {
+	return x - y, nil
+}
+
+func TestMustMax(t *testing.T) {
+	testCases := []struct {
+		name string
+		init Stream[int]
+		want int
+		err  bool
+	}{
+		{
+			name: "empty",
+			init: Stream[int]{elems: []int{}},
+			want: 0,
+			err:  true,
+		},
+		{
+			name: "one element",
+			init: Stream[int]{elems: []int{20}},
+			want: 20,
+			err:  false,
+		},
+		{
+			name: "duplicates",
+			init: Stream[int]{elems: []int{4, 4, 4, 4}},
+			want: 4,
+			err:  false,
+		},
+		{
+			name: "order",
+			init: Stream[int]{elems: []int{10, 5, 15, 20}},
+			want: 20,
+			err:  false,
+		},
+		{
+			name: "negative numbers",
+			init: Stream[int]{elems: []int{-1, -2, -3, -4}},
+			want: -1,
+			err:  false,
+		},
+		{
+			name: "mixed numbers",
+			init: Stream[int]{elems: []int{-1, 2, -3, 4}},
+			want: 4,
+			err:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			defer func() {
+				if err := recover(); (err != nil) != tc.err {
+					t.Errorf("MustMax() recovered panic = %v, wantErr %v", err, tc.err)
+				}
+			}()
+			if got := tc.init.MustMax(compareInts); got != tc.want {
+				t.Errorf("MustMax() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestMustMin(t *testing.T) {
+	testCases := []struct {
+		name        string
+		stream      Stream[int]
+		compareFunc func(x, y int) (int, error)
+		expected    int
+		expectPanic bool
+	}{
+		{
+			name:        "ValidMin",
+			stream:      Of([]int{5, 2, 9, 11, 3}),
+			compareFunc: func(x, y int) (int, error) { return x - y, nil },
+			expected:    2,
+			expectPanic: false,
+		},
+		{
+			name:        "EmptyStream",
+			stream:      Of([]int{}),
+			compareFunc: func(x, y int) (int, error) { return x - y, nil },
+			expected:    0,
+			expectPanic: true,
+		},
+		{
+			name:   "CompareFuncError",
+			stream: Of([]int{5, 2, 9, 11, 3}),
+			compareFunc: func(x, y int) (int, error) {
+				return 0, errors.New("error in comparison")
+			},
+			expected:    2,
+			expectPanic: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			defer func() {
+				r := recover()
+				if r != nil && !tc.expectPanic {
+					t.Errorf("The code panicked, %+v", r)
+				} else if r == nil && tc.expectPanic {
+					t.Error("The code did not panic")
+				}
+			}()
+
+			if result := tc.stream.MustMin(tc.compareFunc); result != tc.expected && !tc.expectPanic {
+				t.Errorf("got %v, want %v", result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestMustFirst(t *testing.T) {
+	testCases := []struct {
+		name string
+		data []int
+		want int
+		err  error
+	}{
+		{
+			name: "Empty",
+			data: []int{},
+			want: 0,
+			err:  errors.New("stream is empty"),
+		},
+		{
+			name: "Single Element",
+			data: []int{1},
+			want: 1,
+			err:  nil,
+		},
+		{
+			name: "Multiple Elements",
+			data: []int{5, 10, 15},
+			want: 5,
+			err:  nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			s := Of(tc.data[:])
+			defer func() {
+				if r := recover(); r != nil {
+					if tc.err == nil {
+						t.Errorf("MustFirst() panic = %v, no panic expected", r)
+					} else if r.(error).Error() != tc.err.Error() {
+						t.Errorf("MustFirst() panic = %v, want panic = %v", r, tc.err.Error())
+					}
+				}
+			}()
+			got := s.MustFirst()
+			if got != tc.want {
+				t.Errorf("MustFirst() = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
+
 func TestMap(t *testing.T) {
 	increment := func(n int) (int, error) {
 		return n + 1, nil
